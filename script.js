4bf45ab2f1e96d5589d2f4357de1f828c1b3b519
Isaac Walsham <109176222+isaacwalsham@users.noreply.github.com>
2025-07-16 08:49:17 +0100

    Updated index.html to add interactions

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const navLinks = document.querySelector('.nav-links');
-
-toggleBtn.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleBtn = document.querySelector(".toggle-btn");
+    const navLinks = document.querySelector(".nav-links");
+  
+    toggleBtn.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  });
